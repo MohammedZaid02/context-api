9bbd208f27cf0a287cd343f67d4afb5d73f63c70
Zaid <zaid@mohammedzaidmz07.com>
2024-09-18 00:54:14 +0530

    Update context api

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import UserContext from '../context/UserContext'
-import { useContext } from 'react'
 
 function Profile() {
-  const {user} = useContext(UserContext)
-  if (!user) return  <div>Please Login</div>
+    const {user} = useContext(UserContext)
     
-  return <div>Welcome {user.username}</div>
+    if (!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
-import { useState } from "react";
 
-const UserContextProvider = ({ children }) => {
-    const [user, SetUser] = useState(null)
-    return (
-        <UserContext.Provider value={{user,SetUser}}>
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
         {children}
         </UserContext.Provider>
     )
 }
 
-export default UserContextProvider;
+export default UserContextProvider
